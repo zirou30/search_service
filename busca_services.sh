709b1ceddc3118b5c8e3d106274c4101dd5eeae5
Zirou <z3rou@hotmail.com>
2018-03-08 20:37:51 -0300

    Update busca_services.sh

--- busca_services.sh
+++ busca_services.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" == "" ]
 	then
-		echo "Search services int /etc/services"
+		echo "Search services in /etc/services"
 		echo "Example of use: $0 ftp"
 	else
 		grep --color -i "^$1" /etc/services
